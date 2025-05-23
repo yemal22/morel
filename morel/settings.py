7bc0e7e112674bbf67c6ae51e0c70db81c5c4d8e
yemal22 <yemalem03@gmail.com>
2025-05-23 20:53:48 +0100

    Remove SECRET_KEY from codebase

--- morel/settings.py
+++ morel/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5st7i2uf=s(m6!qxgfv$2ulrsa(_gw5!^$tf%qzs-e^%r0r4d$'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
