c13839cee29c6d89611f9bc3ede6d87d4bfcff7e
yemal22 <yemalem03@gmail.com>
2025-05-24 06:46:51 +0100

    pipeline ci/cd -- add staticfiles

--- morel/settings.py
+++ morel/settings.py
@@ -127,6 +127,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
